81433f1fb13947802e09329b7400698fc33880aa
SergeyBurlakov49 <srbrmlnv@gmail.com>
2022-10-07 20:03:14 +1100

    i understand smth about my perspectives...

--- Studying/src/Lambda_n_References/ConstructorReference/ConstReference.java
+++ Studying/src/Lambda_n_References/ConstructorReference/ConstReference.java
@@ -2,18 +2,20 @@ package Lambda_n_References.ConstructorReference;
 
 public class ConstReference {
     public static void main(String[] args) {
-        Create obj= A::new;
-        obj.m1();
+        Create obj = A::new;                    /* создается ссылка функционального интерфейса, содержащего в сигнатуре
+                                                   своего метода строку */
+        obj.m1("Constructor with args");    /* реализуется метод m1 через конструктор класса А. Строка будет
+                                                   выведена посредством логики конструктора */
     }
 }
 
 class A{
-    A(){
-        System.out.println("HW");
+    public A(String str){                       // конструктор, выводящий строку из аргумента во время выполнения
+        System.out.println(str);
     }
 }
 
 @FunctionalInterface
 interface Create{
-    void m1();
+    void m1(String str);                        // функциональный интерфейс с методом, принимающим строку
 }
---
+++ Studying/src/Lambda_n_References/PredefinedInterfaces/UnaryOperatorUsage.java
@@ -0,0 +1,14 @@
+package Lambda_n_References.PredefinedInterfaces;
+
+import java.util.function.UnaryOperator;
+
+// функциональный интерфейс UnaryOperator содержит в себе метод apply, предназначенный для того, чтобы обработать
+// объект некоторым образом и вернуть объект того же типа.
+
+public class UnaryOperatorUsage {
+    public static void main(String[] args) {
+        UnaryOperator <String> uo = (str) -> str.toLowerCase(); /* реализация метода apply для строк, которая призвана
+                                                                   переводить все символы в нижний регистр */
+        System.out.println(uo.apply("Hello World"));         // использование сетода для строки "Hello World"
+    }
+}
