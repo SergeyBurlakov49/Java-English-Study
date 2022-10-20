6e33e22e4fad27b79e9d8ddb243fde1ac6c6f7f2
SergeyBurlakov49 <srbrmlnv@gmail.com>
2022-10-21 08:12:33 +1100

    repeating basics (boring)...

--- Studying/src/DataTypes_n_Operators/LightningCalculating/LightningCalculating.java
+++
@@ -1,9 +0,0 @@
-package DataTypes_n_Operators.LightningCalculating;
-
-public class LightningCalculating {
-    public static void main(String[] args) {
-        double time = 9.6;
-        double distance = time * 330;
-        System.out.println("If time of lag between light and sound of lightning = " + time + " then distance = " + distance);
-    }
-}
