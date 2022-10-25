a6da5bed3ee2b52a28ff0622ea6551c4f73038ab
SergeyBurlakov49 <srbrmlnv@gmail.com>
2022-10-26 07:05:46 +1100

    killed coursach

---
+++ Studying/src/Args/Args.java
@@ -0,0 +1,9 @@
+package Args;
+
+public class Args {
+    public static void main(String[] args) {
+        for (String i:args){
+            System.out.println(i);
+        }
+    }
+}
---
+++ Studying/src/ForEachIrregularArray/ForEachIrrlegularArray.java
@@ -0,0 +1,16 @@
+package ForEachIrregularArray;
+
+public class ForEachIrrlegularArray {
+    public static void main(String[] args) {
+        int [][] array =    {{1, 2, 3},
+                            {4, 5, 6},
+                            {7, 8, 9, 0}};
+        int sum = 0;
+        for (int[] i:array){
+            for (int i1:i){
+                sum += i1;
+            }
+        }
+        System.out.println(sum);
+    }
+}
